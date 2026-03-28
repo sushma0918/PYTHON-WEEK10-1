a778202d5445b4530f09db387ded3ec2084c1d42
sushma0918 <sushmamanapuram18@gmail.com>
2026-03-28 20:36:48 +0530

    Add files via upload

---
+++ csvs.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv("C:\\Users\\91798\\PycharmProjects\\.venv\\Lib\\site-packages\\numpy\\_core\\tests\\data\\umath-validation-set-tanh.csv")
+print(df.head())
+print(df.tail())
+print(df.info())
+print(df.describe())
